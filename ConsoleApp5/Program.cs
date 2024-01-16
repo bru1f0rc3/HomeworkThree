906e90cfec0f98696dc9099c9a7f6c86be992464
bru1f0rc3 <wint1841@inbox.ru>
2024-01-16 17:27:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double answer = 0;
+            for(int k = 1; k <= 5; k++)
+            {
+                for (int d = 1; d <= 4; d++) 
+                {
+                    for (int c = 1; c <= 3; c++)
+                    {
+                        answer = Log(k + Pow(d,3) + c);
+                    }
+                }
+            }
+            Console.WriteLine($"Ответ: {answer:f2}");
+        }
+    }
+}
